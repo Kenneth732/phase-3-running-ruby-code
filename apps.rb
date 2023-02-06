518c6134a3a763340fd2f1a0dfb8e4e3c7cfeb89
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-02-06 12:16:48 +0300

    ruby tutoria for beginners

---
+++ apps.rb
@@ -0,0 +1,10 @@
+# 
+puts "Hello World"
+puts ("Hello World")
+
+# print("Hello world! ")
+# print "Hello, Wold! "
+
+puts [1, 2, 3]
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
